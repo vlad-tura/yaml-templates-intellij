866e889880500e6372da279cb23ffa7af7f768d2
GitHub Action <action@github.com>
2022-03-12 11:33:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vladtura/yamltemplatesintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vladtura.yamltemplatesintellij.services
+
+import com.github.vladtura.yamltemplatesintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vladtura/yamltemplatesintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vladtura/yamltemplatesintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vladtura.yamltemplatesintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vladtura.yamltemplatesintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
